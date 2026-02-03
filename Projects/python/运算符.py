cb6fb6eaeb107b36f11c495d39d00bef33dde8fa
zgitw <z_w_ant@163.com>
2026-02-03 15:23:31 +0800

    运算符

--- Projects/python/运算符.py
+++ Projects/python/运算符.py
@@ -28,3 +28,31 @@ a = 10
 b = 3
 c = a//b
 print ("7 - c 的值为：", c);
+
+a = 21 
+b = 10
+
+if ( a == b ):
+    print ("8 - a 等于 b")
+else:
+    print ("8 - a 不等于 b");  
+if ( a != b ):
+    print ("9 - a 不等于 b");
+else:
+    print ("9 - a 等于 b");
+if ( a < b ):
+    print ("10 - a 小于 b");
+else:
+    print ("10 - a 不小于 b");
+if ( a > b ):
+    print ("11 - a 大于 b");
+else:
+    print ("11 - a 不大于 b");
+if ( a <= b ):
+    print ("12 - a 小于或等于 b");
+else:
+    print ("12 - a 大于或等于 b");
+if ( a >= b ):
+    print ("13 - a 大于或等于 b");
+else:
+    print ("13 - a 小于或等于 b");
