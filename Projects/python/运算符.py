486aa877519b1a05aacaf70598162b2c64422911
zgitw <z_w_ant@163.com>
2026-02-03 15:20:30 +0800

    运算符

--- Projects/python/运算符.py
+++ Projects/python/运算符.py
@@ -6,3 +6,25 @@ c = 0
  
 c = a + b
 print ("1 - c 的值为：", c);
+
+c = a - b
+print ("2 - c 的值为：", c);
+
+c = a * b
+print ("3 - c 的值为：", c);
+
+c = a / b
+print ("4 - c 的值为：", c);
+
+c = a % b
+print ("5 - c 的值为：", c);
+
+a = 2
+b = 3
+c = a**b
+print ("6 - c 的值为：", c);
+
+a = 10
+b = 3
+c = a//b
+print ("7 - c 的值为：", c);
