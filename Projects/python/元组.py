6586deac93627534ec5f81dc01112e7e854edb2a
zgitw <z_w_ant@163.com>
2026-02-07 10:08:50 +0800

    元组

---
+++ Projects/python/元组.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+ 
+tup1 = ('Google', 'Runoob', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5, 6, 7 )
+ 
+print ("tup1[0]: ", tup1[0])
+print ("tup2[1:5]: ", tup2[1:5])
+
+#!/usr/bin/python3
+ 
+tup1 = (12, 34.56)
+tup2 = ('abc', 'xyz')
+ 
+# 以下修改元组元素操作是非法的。
+# tup1[0] = 100
+ 
+# 创建一个新的元组
+tup3 = tup1 + tup2
+print (tup3)
+
+#!/usr/bin/python3
+ 
+tup = ('Google', 'Runoob', 1997, 2000)
+ 
+print (tup)
+del tup
+print ("删除后的元组 tup : ")
+print (tup)
+
+# len(tuple)计算元组元素个数。
+# max(tuple)返回元组中元素最大值。
+# min(tuple)返回元组中元素最小值。
+# tuple(iterable)将可迭代系列转换为元组。
