a6b15d4d986fd6043078e10532dc4374f6fc076c
zgitw <z_w_ant@163.com>
2026-02-13 13:16:05 +0800

    lambda

---
+++ Projects/python/lambda.py
@@ -0,0 +1,28 @@
+f = lambda: "Hello, world!"
+print(f())  # 输出: Hello, world!
+
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+numbers = [1, 2, 3, 4, 5]
+squared = list(map(lambda x: x**2, numbers))
+print(squared)  # 输出: [1, 4, 9, 16, 25]
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  # 输出：[2, 4, 6, 8]
+
+from functools import reduce
+ 
+numbers = [1, 2, 3, 4, 5]
+ 
+# 使用 reduce() 和 lambda 函数计算乘积
+product = reduce(lambda x, y: x * y, numbers)
+ 
+print(product)  # 输出：120
